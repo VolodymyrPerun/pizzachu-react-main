9201ab8d4a4015648e27c7f239ea309029af564b
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2021-11-04 00:35:32 +0200

    fix global app wrapper styles

--- src/Components/basics/Header/HeaderContainer.jsx
+++ src/Components/basics/Header/HeaderContainer.jsx
@@ -5,7 +5,6 @@ import { logout } from '../../../redux/reducers/authReducer/thunks'
 //////////////////////////////////////////////////
 
 class HeaderContainer extends React.Component {
-
   render () {
     return (
       <Header {...this.props}/>
